ec3751b02b96532be9c2b51470e01da7aed8faeb
James Grafton <james.grafton9@gmail.com>
2023-12-05 19:07:55 +0000

    train work

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,225 @@
-use std::{fs::File, io::{BufReader, BufRead}, collections::HashMap};
+use std::cmp;
+use std::io::Write;
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 fn main() {
     day_one();
     day_two();
+    day_three();
+}
+
+#[derive(PartialEq)]
+enum Engine {
+    Empty,
+    Symbol,
+    Digit(u32),
+}
+
+fn day_three() {
+
+    let stream = get_file_stream(String::from("engine_scheme"));
+    let scheme: Vec<Vec<Engine>> = stream
+        .lines()
+        .map(|l| map_engine_string_to_engine(l.unwrap()))
+        .collect();
+    let mut head: (usize, usize, bool) = (0, 0, false);
+    let mut tail: (usize, usize) = (0, 0);
+
+    let mut sum = 0;
+
+    for x in 0..(scheme.len() - 1) {
+        for y in 0..(scheme[x].len()) {
+            match scheme[x][y] {
+                Engine::Digit(_n) => {
+                    if tail != (x.checked_sub(1).unwrap_or(0), y.checked_sub(1).unwrap_or(0)) {
+                        head = (x, y, false);
+                    }
+
+                    tail = (x, y);
+                    
+                    if
+                        scheme[x-1][y-1] == Engine::Symbol ||
+                        scheme[x-1][y] == Engine::Symbol ||
+                        scheme[x-1][y+1] == Engine::Symbol ||
+                        scheme[x][y-1] == Engine::Symbol ||
+                        scheme[x][y] == Engine::Symbol ||
+                        scheme[x+1][y+1] == Engine::Symbol ||
+                        scheme[x+1][y] == Engine::Symbol ||
+                        scheme[x+1][y-1] == Engine::Symbol
+                    {
+                        match scheme[x][y + 1] {
+                            Engine::Digit(_) => (),
+                            _ => {
+                                sum += get_num_from_scheme(head, tail, &scheme);
+                            },
+                        }
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+
+    for line in scheme {
+        for place in line {
+            match place {
+                Engine::Empty => print!("."),
+                Engine::Symbol => print!("#"),
+                Engine::Digit(n) => print!("{n}"),
+            }
+        }
+        print!("\n");
+    }
+
+    print!("day three: {sum}");
+}
+
+fn get_num_from_scheme(
+    head: (usize, usize, bool),
+    tail: (usize, usize),
+    scheme: &Vec<Vec<Engine>>,
+) -> u32 {
+    let mut total = 0;
+    for i in 0..(head.1 - tail.1) {
+        match scheme[head.0][i] {
+            Engine::Digit(n) => total += n,
+            _ => (),
+        }
+    }
+
+    total
+}
+
+fn map_engine_string_to_engine(line: String) -> Vec<Engine> {
+    line.chars()
+        .into_iter()
+        .map(|c| match c {
+            '.' => Engine::Empty,
+            '*' | '&' | '#' | '+' | '/' | '=' | '-' | '\\' | '@' | '%' | '$' => {
+                return Engine::Symbol
+            }
+            n => Engine::Digit(n.to_digit(10).unwrap()),
+        })
+        .collect()
 }
 
 fn day_two() {
+    let stream = get_file_stream(String::from("cube_conundrum"));
+    let result = stream
+        .lines()
+        .map(|l| parse_cube_game_day(l.unwrap()))
+        .collect::<Vec<_>>();
+    let real_result = result
+        .into_iter()
+        .fold(0, |acc, r| acc + game_is_impossible_id_total_for_day(r));
+    print!("day two answer: {}", real_result);
+}
 
+struct CubeGame {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+fn game_is_impossible_id_total_for_day(day: (usize, Vec<CubeGame>)) -> u32 {
+    let mut max_cube = CubeGame {
+        red: 0,
+        green: 0,
+        blue: 0,
+    };
+    for cb in day.1 {
+        max_cube.red = cmp::max(max_cube.red, cb.red);
+        max_cube.green = cmp::max(max_cube.green, cb.green);
+        max_cube.blue = cmp::max(max_cube.blue, cb.blue);
+    }
+    max_cube.red * max_cube.green * max_cube.blue
+}
+
+fn parse_cube_game_day(line: String) -> (usize, Vec<CubeGame>) {
+    let mut split = line.split(":");
+    let index = split.next().unwrap();
+    let day = split.next().unwrap();
+    let index: usize = index.split(" ").collect::<Vec<&str>>()[1]
+        .trim()
+        .parse()
+        .unwrap();
+
+    let day: Vec<&str> = day.split(";").collect();
+    let mut cube_games: Vec<CubeGame> = Vec::new();
+
+    for game in day {
+        let round: HashMap<&str, u32> = game
+            .split(",")
+            .map(|s| {
+                let spl: Vec<&str> = s.split(" ").collect();
+                (spl[2], spl[1].parse().unwrap())
+            })
+            .collect();
+
+        let cube_game = CubeGame {
+            red: *round.get(&"red").unwrap_or(&0),
+            green: *round.get(&"green").unwrap_or(&0),
+            blue: *round.get(&"blue").unwrap_or(&0),
+        };
+
+        cube_games.push(cube_game);
+    }
+
+    (index, cube_games)
 }
 
 fn day_one() {
     let stream = get_file_stream(String::from("calibration"));
-    let result = stream.lines().map(
-        |l| select_only_digits(l.unwrap())
-    ).fold(
-        0, |acc,x| acc + get_first_and_last_digits(x)
-    );
-    print!("dayone answer: {result}\n");
+    let result = stream
+        .lines()
+        .map(|l| select_only_digits(l.unwrap()))
+        .fold(0, |acc, x| acc + get_first_and_last_digits(x));
+    print!("day one answer: {result}\n");
 }
 
 fn select_only_digits(input: String) -> Vec<u32> {
     let numbers = HashMap::from([
-        ("zero",0),("0",0),
-        ("one",1),("1",1),
-        ("two",2),("2",2),
-        ("three",3),("3",3),
-        ("four",4),("4",4),
-        ("five",5),("5",5),
-        ("six",6),("6",6),
-        ("seven",7),("7",7),
-        ("eight",8),("8",8),
-        ("nine",9),("9",9)
+        ("zero", 0),
+        ("0", 0),
+        ("one", 1),
+        ("1", 1),
+        ("two", 2),
+        ("2", 2),
+        ("three", 3),
+        ("3", 3),
+        ("four", 4),
+        ("4", 4),
+        ("five", 5),
+        ("5", 5),
+        ("six", 6),
+        ("6", 6),
+        ("seven", 7),
+        ("7", 7),
+        ("eight", 8),
+        ("8", 8),
+        ("nine", 9),
+        ("9", 9),
     ]);
-    let mut number_indices: HashMap<usize,u32> = HashMap::new();
+    let mut number_indices: HashMap<usize, u32> = HashMap::new();
 
-    for (word,number) in numbers {
+    for (word, number) in numbers {
         let matches: Vec<(usize, &str)> = input.match_indices(word).collect();
-        for (index,_) in matches {
+        for (index, _) in matches {
             number_indices.insert(index, number);
         }
     }
-    
+
     let mut as_vec: Vec<(usize, u32)> = number_indices.into_iter().collect();
-    as_vec.sort_by(|(k1, _),(k2, _)| k1.cmp(k2));
-    as_vec.into_iter().map(
-        |(_, number)| number
-    )
-    .collect()
+    as_vec.sort_by(|(k1, _), (k2, _)| k1.cmp(k2));
+    as_vec.into_iter().map(|(_, number)| number).collect()
 }
 
 fn get_file_stream(filename: String) -> BufReader<File> {
-    let file = File::open(
-        format!("files/{filename}"
-    )).unwrap();
+    let file = File::open(format!("files/{filename}")).unwrap();
     BufReader::new(file)
 }
 
